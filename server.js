ea8e51bb662cd9f7a0b87fad9c6791d5b4212a0f
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-11-21 09:57:58 -0500

    working on routes

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-// Dependencies
 const express = require("express");
 const fs = require("fs");
-const db = require('./db/db.json');
 
 const app = express();
 
@@ -12,6 +10,13 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let jsonData = {}
+fs.readFile('db/db.json', 'utf-8', (err, data) => {
+  if (err) throw err
+  
+  jsonData = JSON.parse(data)
+})
+
 // Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -20,6 +25,33 @@ app.get("/notes", function(req, res) {
 app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
+
+app.get(`/api/notes`, function(req,res) {
+    res.sendFile(path.join(__dirname, "db/db.json"));
+})
+
+app.post('/api/notes', function(req, res) {
+    let newNotes = req.body;
+  
+    newNotes.routeName = newNotes.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newNotes);
+  
+    db.push(newNotes);
+  
+    res.json(newNotes);
+});
+
+app.delete('/api/notes/:id', function(req, res) {
+    let removeUser = req.body.id;
+    console.log(req.body, "left is req body", req.body.id, "left of this is req body id")
+    let data = fs.readFileSync('db/db.json');
+    let json = JSON.parse(data);
+    let id = json.id;
+    json.id = id.filter((id) => { return id.id !== removeUser });
+    fs.writeFileSync('results.json', JSON.stringify(json, null, 2));
+})
+
 // open listing port for server
 app.listen(PORT, function() {
 
