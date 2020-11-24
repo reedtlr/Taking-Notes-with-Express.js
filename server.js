40597f76e8d5c579778e143da35d192da2219533
reedtlr <69362002+reedtlr@users.noreply.github.com>
2020-11-24 13:59:06 -0500

    fixed routes and added uuid npm

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const fs = require("fs");
-
+const path = require("path");
 const app = express();
+const { v4: uuidv4 } = require('uuid');
+
+let dbJson = require("./db/db.json");
 
 // process.env is being used so it can be deployed on Heroku
 const PORT = process.env.PORT || 8080;
@@ -11,46 +14,41 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-let jsonData = {}
-fs.readFile('db/db.json', 'utf-8', (err, data) => {
-  if (err) throw err
+// let jsonData = {}
+// fs.readFile('db/db.json', 'utf-8', (err, data) => {
+//   if (err) throw err
   
-  jsonData = JSON.parse(data)
-})
+//   jsonData = JSON.parse(data)
+// })
 
 // Routes
 app.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
   });
   
-app.get("*", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
   });
 
-app.get(`/api/notes`, function(req,res) {
-    res.sendFile(path.join(__dirname, "db/db.json"));
+app.get("/api/notes", function(req,res) {
+  res.json(dbJson);
 })
 
-app.post('/api/notes', function(req, res) {
+app.post("/api/notes", function(req, res) {
     let newNotes = req.body;
-  
-    newNotes.routeName = newNotes.name.replace(/\s+/g, "").toLowerCase();
-  
-    console.log(newNotes);
-  
-    db.push(newNotes);
-  
-    res.json(newNotes);
+    let id = uuidv4();
+    newNotes.id = id;
+    
+    dbJson.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbJson));
+    res.json(dbJson);
 });
 
 app.delete('/api/notes/:id', function(req, res) {
-    let removeNote = req.body.id;
-    console.log(req.body, "left is req body", req.body.id, "left of this is req body id")
-    let data = fs.readFileSync('db/db.json');
-    let json = JSON.parse(data);
-    let id = json.id;
-    json.id = id.filter((id) => { return id.id !== removeNote });
-    fs.writeFileSync('results.json', JSON.stringify(json, null, 2));
+    let currentNotes = dbJson.filter(note => note.id != req.params.id)
+    dbJson = currentNotes
+    fs.writeFileSync('./db/db.json', JSON.stringify(dbJson));
+    res.json(dbJson);
 })
 
 // open listing port for server
